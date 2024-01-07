b8717832d39e401afc05ddc2d00e2a0b032638d9
martinpfh <martin@pfh.de>
2024-01-07 17:44:52 +0100

    Add global fixture in conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+#
+# dlv:tests:conftest
+#
+import pytest
+
+@pytest.fixture(autouse=True, name="generate_cusips")
+def generate_cusips():
+    return [
+        '9128286Y1',
+        '912828XW5',
+        '9128287C8',
+        '9128282P4',
+        '912828YA2',
+        '9128282S8',
+        '912828YF1',
+        '9128282W9',
+    ]
+
