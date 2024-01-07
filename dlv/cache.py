b6d28b89f86567973dac4e56105a5f6b4b5a3d06
martinpfh <martin@pfh.de>
2024-01-07 17:45:21 +0100

    Add test for the cache module

--- dlv/cache.py
+++ dlv/cache.py
@@ -10,12 +10,32 @@ from .future import Future
 CACHE_DIRECTORY: Final = '.bcache'
 
 class Cache():
+    def __init__(self, future: Future, basket: Basket):
+        self.future = future
+        self.basket = basket
+
+    def put(self):
+        if not self.create_cache_dir_if_not_exits():
+            raise ValueError(f'Cant create cach directory: {self.get_cache_directory()}')
+        
+        fname = self.get_cache_directory() + os.sep + self.get_filename() + '.yaml'
+        return self.basket.serialize(filename=fname)
+    
+    def get_filename(self) -> str:
+        return (self.future.long_code + self.basket.hashcode()).lower()
+
     def cache_basket(self, future: Future, basket: Basket) -> bool:
         # basket.serialize()
+        if not self.create_cache_dir_if_not_exits():
+            raise ValueError(f'Cant create cach directory: {self.get_cache_directory()}')
+
         return True
 
+    def get_cache_directory(self) -> str:
+        return os.curdir + os.sep + CACHE_DIRECTORY
+
     def create_cache_dir_if_not_exits(self) -> str:
-        dirname = os.curdir + os.sep + CACHE_DIRECTORY
+        dirname = self.get_cache_directory()
 
         if os.path.exists(dirname):
             return dirname
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -2,5 +2,17 @@
 # dlv:tests:cache
 #
 
-def test_cache():
-    pass
+from dlv.basket import Basket
+from dlv.cache import Cache
+from dlv.future import Future
+
+from .basket_fixture import create_basket
+from .future_fixture import create_future
+
+def test_cache_basket(create_future: Future, create_basket: Basket):
+    future = create_future
+    basket = create_basket
+    c = Cache(future=future, basket=basket)
+    assert c.put()
+
+    # c.cache_basket(basket)
