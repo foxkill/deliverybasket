d04ecce1fcda8859a7cc2a49be247a9bf9c3ae72
martinpfh <martin@pfh.de>
2024-01-10 12:38:28 +0100

    Add rate limiting to httpx requests

--- dlv/thttp.py
+++ dlv/thttp.py
@@ -1,19 +1,21 @@
 #
 # dlv:http
 # 
-from typing import Iterable
+from typing import Final, Iterable
 import httpx as requests
 import asyncio
 
-__search_url__ = 'https://www.treasurydirect.gov/TA_WS/securities/search'
+TIMEOUT = 10
+MAX_CONNECTIONS = 8
 
-async def fetch(url):
-    await asyncio.sleep(1/1000)
-    return requests.get(url)
+__search_url__: Final = 'https://www.treasurydirect.gov/TA_WS/securities/search'
 
-async def get(cusips: Iterable[str]):
-    urls = [url(cusip) for cusip in cusips]
-    responses = await asyncio.gather(*[fetch(url) for url in urls])
+async def fetch(client, url):
+    return client.get(url)
+
+async def get(cusips: Iterable[str]) -> list[requests.Response]:
+    client = requests.Client(timeout=TIMEOUT, limits=requests.Limits(max_connections=MAX_CONNECTIONS))
+    responses = await asyncio.gather(*[fetch(client, url(cusip)) for cusip in cusips])
     return responses
 
 def url(cusip):
