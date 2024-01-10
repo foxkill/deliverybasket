e939d3fff34edca641630ac13499e4d37e4da2ac
martinpfh <martin@pfh.de>
2024-01-10 12:38:50 +0100

    Check if all response where 200s

--- tests/test_thttp.py
+++ tests/test_thttp.py
@@ -4,8 +4,12 @@
 import pytest
 from dlv.thttp import get
 
+NUMER_OF_CUSIPS = 8
+
 @pytest.mark.asyncio
 async def test_get_cusips(generate_cusips):
-    cusips = [generate_cusips for i in range(8)]
+    cusips = generate_cusips[0:NUMER_OF_CUSIPS]
     r = await get(cusips)
+    codes = list(filter(lambda x: (x.status_code == 200), r))
     assert len(r) == 8
+    assert len(codes) == 8
