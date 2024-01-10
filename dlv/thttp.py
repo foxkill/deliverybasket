7dabc0fb96b8d7236cd8acc7fd7c52cb72d0bf92
martinpfh <martin@pfh.de>
2024-01-10 12:07:45 +0100

    Add httpx library for getting requests done.

--- dlv/thttp.py
+++ dlv/thttp.py
@@ -1,15 +1,17 @@
 #
 # dlv:http
 # 
-import requests
+from typing import Iterable
+import httpx as requests
 import asyncio
 
 __search_url__ = 'https://www.treasurydirect.gov/TA_WS/securities/search'
 
 async def fetch(url):
+    await asyncio.sleep(1/1000)
     return requests.get(url)
 
-async def get(cusips):
+async def get(cusips: Iterable[str]):
     urls = [url(cusip) for cusip in cusips]
     responses = await asyncio.gather(*[fetch(url) for url in urls])
     return responses
