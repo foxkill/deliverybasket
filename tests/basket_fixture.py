f3fcd1c731d8ccd988ebdc175ab0a2efe30947c3
martinpfh <martin@pfh.de>
2024-01-11 18:59:16 +0100

    Fix changed parameter list.

--- tests/basket_fixture.py
+++ tests/basket_fixture.py
@@ -8,7 +8,7 @@ from dlv.basket import Basket
 @pytest.fixture
 def create_basket(create_future, generate_cusips) -> Basket:
     tr = {t:None for t in generate_cusips}
-    basket = Basket(create_future, tr)  # type: ignore
+    basket = Basket(tr)  # type: ignore
     return basket
 # 'Object of type coroutine is not JSON serializable
 @pytest.fixture
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -7,6 +7,7 @@ from io import StringIO
 from unittest.mock import mock_open, patch
 from dlv.basket import Basket
 from dlv.cache import Cache
+from dlv.future import Future
 from .basket_fixture import create_basket
 
 # def test_open(mocker):
@@ -17,8 +18,10 @@ from .basket_fixture import create_basket
 #     m.assert_called_once_with('foo')
 #     assert result == 'bibble'
 
-def test_cache_put(create_basket: Basket, mocker):
+def test_cache_put(create_basket: Basket, create_future: Future):
     basket = create_basket
+    basket.future = str(create_future)
+    
     c = Cache()
 
     with patch('dlv.cache.open') as mo:
@@ -26,18 +29,18 @@ def test_cache_put(create_basket: Basket, mocker):
         mo.side_effect = [out]
         r =  c.put(basket)
         assert r == True
-        assert out.mock_calls[1].args[0].strip() == 'future: ' + basket._future.long_code
+        assert out.mock_calls[1].args[0].strip() == 'future: ' + basket.future.long_code
 
 def test_cache_get(create_future):
     c = Cache()
     with patch('dlv.basket.open', mock_open(read_data='future: ulh2024')) as mo:
-        basket = c.get(c.get_filename(create_future.long_code))
+        basket = c.get(create_future.long_code)
         assert not basket is None
-        assert basket.get_future().long_code == 'ULH2024'
+        assert basket.future.long_code == 'ULH2024'
         assert basket.has_basket() == False
 
 def test_cache_get_with_invalid_filename():
     c = Cache()
     filename = 'x'
-    basket = c.get(filename)
-    assert basket is None
+    with pytest.raises(FileNotFoundError):
+        basket = c.get(filename)
