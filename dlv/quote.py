2bc1b042897ff414fe108deac5d2fc48120fc73e
martinpfh <martin@pfh.de>
2024-01-14 17:51:25 +0100

    Add more tests and functionality to parse treasury quoted prices

--- dlv/quote.py
+++ dlv/quote.py
@@ -10,8 +10,10 @@ from typing import Final
 class QuoteStyle(enum.IntFlag):
     DETECT = 0
     BOND = 1
-    NOTE_FUTURE = 2
-    BOND_FUTURE = 3
+    SHORT_NOTE_FUTURE = 3
+    NOTE_FUTURE = 4
+    BOND_FUTURE = 5
+    DECIMAL = 6
 
 FRACTION32_BOND: Final = {
     0: 0,
@@ -29,6 +31,35 @@ FRACTION32_NOTE: Final = {
     5: 1/2,
     7: 3/4
 }
+
+FRACTION32_SHORT_TERM_NOTE = {
+    1: 1/32/8,
+    2: 2/32/8,
+    3: 3/32/8,
+    4: 4/32/8,
+    5: 5/32/8,
+    6: 6/32/8,
+    7: 8/32/8,
+}
+
+def parse_short_term_note_future_price(number: str, fraction: str, fraction32: str) -> float:
+    """Parse the price of a note future, TN, ZN, ZF, ZT"""
+    price = 0
+    if number.isnumeric():
+        price += int(number)
+
+    if fraction32.isnumeric() and not (fraction32.isnumeric() or fraction32 != '+'):
+        price += (float(fraction) / 32)
+    else:    
+        fraction_index = int(fraction32)
+
+        debug =  FRACTION32_SHORT_TERM_NOTE.get(fraction_index, 0)
+        price += \
+            (int(fraction)/32) + \
+            FRACTION32_SHORT_TERM_NOTE.get(fraction_index, 0)
+
+    return price
+
 
 def parse_note_future_price(number: str, fraction: str, fraction32: str) -> float:
     """Parse the price of a note future, TN, ZN, ZF, ZT"""
@@ -86,24 +117,30 @@ def parse_tresury_price(number: str, fraction: str, fraction32: str) -> float:
 
     return price
 
-def parse_none(number: str, fraction: str, fraction32: str) -> float:
-    price = 0
-    if number.isnumeric():
-        price += float(number)
-    if fraction.isnumeric():
-        price += float(fraction)
-    return price
+def parse_decimal(number: str, fraction: str, fraction32: str) -> float:
+    price_str = '%s.%s%s' % (number, fraction, fraction32)
+    return float(price_str)
 
-def detect_quote_style(delimiter_frac: str, delimter32: str) -> QuoteStyle:
+def parse_none(number: str, fraction: str, fraction32: str) -> float:
+    return 0
+
+def detect_quote_style(delimiter_frac: str, delimiter32: str) -> QuoteStyle:
     if not delimiter_frac is None and delimiter_frac == '.':
-        return QuoteStyle.DETECT
+        return QuoteStyle.DECIMAL
+    if (not delimiter_frac is None and delimiter_frac == "'"):
+        if (not delimiter32 is None and delimiter32 == "'"):
+            return QuoteStyle.NOTE_FUTURE
+        else:
+            return QuoteStyle.BOND_FUTURE
 
     return QuoteStyle.BOND
 
 PARSER: Final = {
     QuoteStyle.BOND: parse_tresury_price,
     QuoteStyle.BOND_FUTURE: parse_bond_future_price,
-    QuoteStyle.NOTE_FUTURE: parse_note_future_price
+    QuoteStyle.NOTE_FUTURE: parse_note_future_price,
+    QuoteStyle.SHORT_NOTE_FUTURE: parse_short_term_note_future_price,
+    QuoteStyle.DECIMAL: parse_decimal,
 }
 
 @dataclass
@@ -113,7 +150,7 @@ class Quote:
     @classmethod
     def parse(cls, quote: str, quotestyle = QuoteStyle.DETECT):
         price = 0
-        regex = r"(?P<number>\d+)(?P<delimiter_frac>[\.\-\'])(?P<fraction>\d{2})(?P<delimter32>'?)(?P<fraction32>[\d\+])?"
+        regex = r"(?P<number>^\d+)(?P<delimiter_frac>[\.\-\'])?(?P<fraction>\d{2})?(?P<delimiter32>'?)(?P<fraction32>\d\+)?"
         matches = re.finditer(regex, quote, re.MULTILINE)
 
         for matchnum, match in enumerate(matches, start=1):
@@ -122,7 +159,7 @@ class Quote:
             fraction32 = match.group('fraction32')
 
             style = quotestyle if quotestyle != QuoteStyle.DETECT else \
-                detect_quote_style(match.group('delimter_frac3'), match.group('delimiter32'))
+                detect_quote_style(match.group('delimiter_frac'), match.group('delimiter32'))
 
             number = '0' if number is None else number
             fraction = '0' if fraction is None else fraction
--- tests/test_quote.py
+++ tests/test_quote.py
@@ -7,7 +7,9 @@ from dlv.quote import Quote, QuoteStyle
 @pytest.mark.parametrize(
     "value, style, expected",
     [
+        ('103\'03\'3', QuoteStyle.SHORT_NOTE_FUTURE, 103.1171875),
         ('110.11', QuoteStyle.DETECT, 110.11),
+        ('110', QuoteStyle.DETECT, 110),
         ('110-11', QuoteStyle.NOTE_FUTURE, 110.34375),
         ('103-257', QuoteStyle.BOND, 103.80859375),
         ('tum4', QuoteStyle.BOND, 0),
